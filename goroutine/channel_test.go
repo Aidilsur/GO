52b42854cc081470c160a4440b03960ad2d0a36f
saidil <aidilsurya9@gmail.com>
2025-10-29 13:45:07 +0700

    channel

---
+++ goroutine/channel_test.go
@@ -0,0 +1,29 @@
+package goroutine
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestCreateChannel(t *testing.T) {
+	channel := make(chan string);
+	defer close(channel)
+
+	// channel <- "Aidil"
+	// data := <- channel
+
+	// fmt.Println(data)
+	// defer close(channel)
+
+	go func() {
+		time.Sleep((2 * time.Second))
+		channel <- "Aidil Surya"
+
+		fmt.Println("selesai mengirim data ke channel")
+	} ()
+
+	data := <- channel
+	fmt.Println(data)
+	time.Sleep(5 * time.Second)
+}
