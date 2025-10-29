20e6ee181a8ed4c493c8ccbff11e7fee097b734d
saidil <aidilsurya9@gmail.com>
2025-10-29 14:37:49 +0700

    default select

--- goroutine/channel_test.go
+++ goroutine/channel_test.go
@@ -148,3 +148,33 @@ func TestSelectChannel(t *testing.T) {
 		} 
 	}
 }
+
+// default channel
+func TestDefaultChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+
+	for {
+		select {
+		case data:= <-channel1:
+			fmt.Println("Data dari channel 1", data)
+			counter++
+
+		case data:= <- channel2:
+			fmt.Println("Data dari Channel 2", data)
+			counter++
+
+		default: 
+			fmt.Println("Menunggu Data")
+		}
+
+		if counter == 2 {
+			break
+		} 
+	}
+}
