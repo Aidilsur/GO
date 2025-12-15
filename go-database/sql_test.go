b6ccc4cddab5aa388b86471d7fe7b3f0f63806bf
saidil <aidilsurya9@gmail.com>
2025-12-15 11:10:55 +0700

    auto increment

--- go-database/sql_test.go
+++ go-database/sql_test.go
@@ -154,3 +154,27 @@ func TestExecSqlParameter(t *testing.T) {
 
 	fmt.Println("Success insert new customer")
 }
+
+func TestAutoIncrement(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	email := "dils@mail.com"
+	comment := "test comment 4"
+
+	script := "INSERT INTO comments(email, comment) VALUES(?, ?)"
+	result, err := db.ExecContext(ctx, script, email, comment)
+
+	if err != nil {
+		panic(err)
+	}
+
+	insertId, err :=result.LastInsertId()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Success insert new comment with id", insertId)
+}
