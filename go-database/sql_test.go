fa15307a8ce04eb10018a5c7ca79dec61e840139
saidil <aidilsurya9@gmail.com>
2025-12-15 10:50:51 +0700

    sql injection

--- go-database/sql_test.go
+++ go-database/sql_test.go
@@ -2,8 +2,10 @@ package go_database
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestExecSql(t *testing.T) {
@@ -14,6 +16,137 @@ func TestExecSql(t *testing.T) {
 
 	script := "INSERT INTO customer(id, name) VALUES('aidils', 'Aidil')"
 	_, err := db.ExecContext(ctx, script)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Success insert new customer")
+}
+
+func TestQuerySql(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	script := "SELECT id, name FROM customer"
+	rows, err := db.QueryContext(ctx, script)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var id, name string
+		err = rows.Scan(&id, &name)
+
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("id :", id)
+		fmt.Println("name :", name)
+	}
+}
+
+func TestQuerySqlComplex(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	script := "SELECT id, name, email, balance, rating, birth_date, married, created_at  FROM customer"
+	rows, err := db.QueryContext(ctx, script)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var id, name string
+		var email sql.NullString
+		var balance int32
+		var rating float64
+		var created_at time.Time
+		var birth_date sql.NullTime
+		var married bool
+
+		err = rows.Scan(&id, &name, &email, &balance,&rating, &birth_date, &married, &created_at)
+
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("====================================")
+		fmt.Println("id :", id)
+		fmt.Println("name :", name)
+		if email.Valid {
+			fmt.Println("email :", email.String)
+		}
+		fmt.Println("balance :", balance)
+		fmt.Println("rating :", rating)
+		if birth_date.Valid {
+			fmt.Println("birth_date :", birth_date.Time)
+		}
+		fmt.Println("married :", married)
+		fmt.Println("created_at :", created_at)
+	}
+}
+
+func TestSqlInjection(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	// username := "admin' OR '1'='1" // contoh salah
+	username := "admin"
+	password := "admin"
+
+	// script := "SELECT username FROM user WHERE username = '" + username + "' AND password = '" + password +"' LIMIT 1" // contoh salah
+
+	script := "SELECT username FROM user WHERE username = ? AND password = ? LIMIT 1" 
+	
+	// rows, err := db.QueryContext(ctx, script) // menggunakan script pertama atau script yang salah
+	
+	rows, err := db.QueryContext(ctx, script, username, password)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer rows.Close()
+
+	if rows.Next() {
+		var username string
+		err := rows.Scan(&username)
+
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Sukses Login", username)
+	} else {
+		fmt.Println("Gagal Login")
+	}	
+}
+
+func TestExecSqlParameter(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	username := "dils"
+	password := "dils"
+
+	script := "INSERT INTO user(username, password) VALUES(?, ?)"
+	_, err := db.ExecContext(ctx, script, username, password)
 
 	if err != nil {
 		panic(err)
