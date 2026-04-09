fa3f934617ba015359ea09715fdd2d69597f9278
aidilsur <aidilsurya9@gmail.com>
2026-04-09 11:07:37 +0700

    go-web/template layout

---
+++ go-web/template_layout_test.go
@@ -0,0 +1,27 @@
+package goweb
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TemplateLayout(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles("templates/header.gohtml", "templates/footer.gohtml", "templates/layout.gohtml"))
+	t.ExecuteTemplate(w, "layout", map[string]interface{}{
+		"Title": "Template Layout",
+		"Name": "Aidil",
+	})
+}
+
+func TestTemplateLayout(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateLayout(recorder, request)
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
